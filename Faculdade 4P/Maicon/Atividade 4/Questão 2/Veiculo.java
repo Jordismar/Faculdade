e635f0fcaf3c7f5feed1a7b1cd43ddae23925f2f
Jordismar <97907556+Jordismar@users.noreply.github.com>
2022-09-14 22:08:26 -0300

    maicon

---
+++ Faculdade 4P/Maicon/Atividade 4/Questão 2/Veiculo.java
@@ -0,0 +1,12 @@
+public class Veiculo {
+  private String nome;
+  private String modelo;
+  private int cavalos;
+  
+  public Veiculo(){
+    nome = "";
+    modelo = "";
+    cavalos = 0;
+  }
+  
+}
