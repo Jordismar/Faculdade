e299921a30d2a3b6c220896e8cca86ed5345aba2
Jordismar <97907556+Jordismar@users.noreply.github.com>
2022-09-27 20:33:06 -0300

    Finalizando questão 2 do elias

--- Faculdade 4P/Elias/Atividade 4/Questão 2/Agenda.java
+++ Faculdade 4P/Elias/Atividade 4/Questão 2/Agenda.java
@@ -1,16 +1,62 @@
 public class Agenda {
     public Pessoa[] p;
+    public int aux;
 
     public Agenda(int tamVet) {
         p = new Pessoa[tamVet];
+        aux = 0;
     }
-    public Pessoa getP() {
-        return p;
-    }
-    public void setP(Pessoa p) {
-        this.p = p;
-    }
-    public void armazeaPessoa(int posicao){
 
+    public void setP(Pessoa p, int posicao) {
+        this.p[posicao] = p;
+    }
+
+    public void imprimePessoa(int posicao) {
+        if (p[posicao] != null) {
+            System.out.println("============================");
+            System.out.println("Pessoa [" + posicao + "]");
+            System.out.println("============================");
+            System.out.println("Nome ->" + p[posicao].getNome());
+            System.out.println("Idade ->" + p[posicao].getIdade());
+            System.out.println("Altura ->" + p[posicao].getAltura());
+        }
+    }
+
+    public void imprimeVetorPessoa(int quantP) {
+        if (quantP == 0) {
+            System.out.println("============================");
+            System.out.println("So existe esta Pessoa");
+            System.out.println("============================");
+            imprimePessoa(0);
+        } else {
+            for (int i = 0; i < quantP; i++) {
+                imprimePessoa(i);
+            }
+        }
+    }
+
+    public int buscaPessoa(String nome, int quantP) {
+        for (int i = 0; i < quantP; i++) {
+            if (nome == p[i].getNome()) {
+                aux = i;
+            }
+        }
+        return aux;
+    }
+
+    public int retirarPessoa(String nome, int quantP) {
+
+        aux = buscaPessoa(nome, quantP);
+        if (quantP == 0) {
+            p[0] = null;
+        } else {
+            for (int i = 0; i < quantP; i++) {
+                if (aux >= i && p[aux + 1] != null) {
+                    p[aux] = p[aux + 1];
+                }
+            }
+            quantP--;
+        }
+        return quantP;
     }
 }
--- Faculdade 4P/Elias/Atividade 4/Questão 2/Main.java
+++ Faculdade 4P/Elias/Atividade 4/Questão 2/Main.java
@@ -1,35 +1,60 @@
 import java.util.Scanner;
 
+import javax.sound.midi.Soundbank;
+
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int op = 0;
+        int quantP = 0;
+        String aux = "";
         Pessoa p;
         int tamVet = 10;
         Agenda a = new Agenda(tamVet);
         do {
+            System.out.println("\n");
             System.out.println("1- Cadastrar Pessoa");
-            System.out.println("2- Exibir agenda");
+            System.out.println("2- Exibir agenda completa");
+            System.out.println("3- Exibir Pessoa");
+            System.out.println("4- Remover uma Pessoa");
             System.out.println("0- Sair");
             System.out.print("Selecione -> ");
             op = entrada.nextInt();
+            entrada.nextLine();
+            System.out.println("\n");
             int i = 0;
             switch (op) {
                 case 1:
+                    p = new Pessoa();
                     System.out.print("Nome-> ");
                     p.setNome(entrada.nextLine());
                     System.out.print("Idade-> ");
                     p.setIdade(entrada.nextInt());
                     System.out.print("Altura-> ");
                     p.setAltura(entrada.nextFloat());
-                    a.armazeaPessoa(i);
-                    i++;
+                    a.setP(p, quantP);
+                    quantP++;
+                    System.out.println("\n");
                     break;
                 case 2:
+                    a.imprimeVetorPessoa(quantP);
+                    System.out.println("\n");
+                    break;
+                case 3:
+                    System.out.print("Informe a posicao da pessoa ->");
+                    i = entrada.nextInt();
+                    a.imprimePessoa(i);
+                    System.out.println("\n");
+                    break;
+                case 4:
+                    System.out.println("Informe o nome de quem deseja exluir:");
+                    aux = entrada.nextLine();
+                    quantP = a.retirarPessoa(aux, quantP);
 
                     break;
                 default:
                     System.out.println("Selecione Corretamente\n");
+                    System.out.println("\n");
                     break;
             }
         } while (op != 0);
